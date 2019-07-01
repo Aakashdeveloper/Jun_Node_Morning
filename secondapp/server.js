5d5e1c02269ff306b10e59c0ab9dda982c0eb02e
aakashDeveloper <ahanda205@gmail.com>
2019-07-01 02:27:44 +0100

    a

--- secondapp/server.js
+++ secondapp/server.js
@@ -13,10 +13,35 @@ app.get('/', (req,res) => {
     res.status(200).send('hi to browser')
 })
 
+let final_menu = [{name:'Home', link:"/home"},
+                    {name:'Product',link:'/products'},
+                    {name:'Order', link:"/orders"}];
+
 app.get('/home', (req,res) => {
     // res.status(200).send('this is home page')
-    res.render('home',{title:'Welcome to ejs'})
+    res.render('home',{title:'Home Page', 
+                      menu:final_menu})
 })
+
+app.get('/products', (req,res) => {
+    res.render('Products',{title:'Product Page',
+                        menu:final_menu})
+})
+
+app.get('/orders', (req,res) => {
+    res.render('order',{title:'Order Page',
+                        menu:final_menu})
+})
+app.get('/details', (req,res) => {
+    res.render('order_Details',{title:'Orders Details Page',
+                        menu:final_menu})
+})
+
+app.get('/details', (req,res) => {
+    res.render('product_details',{title:'Product Details Page',
+                        menu:final_menu})
+})
+
 
 app.listen(port, (err)=>{
     if(err){
