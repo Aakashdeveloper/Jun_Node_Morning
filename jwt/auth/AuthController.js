bfbd042fb9dccb3581220ad5cfb52dcf2de67082
aakashDeveloper <ahanda205@gmail.com>
2019-07-24 02:20:06 +0100

    a

--- jwt/auth/AuthController.js
+++ jwt/auth/AuthController.js
@@ -26,7 +26,33 @@ router.post('/register', (req,res) => {
            res.status(200).send({auth:true, token:token})
         }
     )
+});
+
+router.post('/login', (req,res) => {
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err) return res.status(500).send('Error');
+        if(!user) return res.status(404).send("No user found")
+        let passwordIsValid = bycrpt.compareSync(req.body.password, user.password);
+        if(!passwordIsValid)return  res.status(401).send({auth:false, token:null})
+        var token = jwt.sign({id:user._id}, config.secert, {
+            expiresIn:86400
+        })
+        res.status(200).send({auth:true,token:token});
+    })
+});
+
+router.get('/userinfo', (req,res)=>{
+    var token = req.headers['x-access-token'];
+    if(!token) return res.status(401).send({auth:false, message:'no token found'})
+    jwt.verify(token, config.secert, (err,decode) =>{
+        if(err) return res.status(500).send({auth:false, message: 'Fail'})
+        User.findById(decode.id, {password:0}, (err,user)=>{
+            if(err) return res.status(500).send('No user found')
+            res.status(200).send(user)
+        })
+    })
 })
+
 
 
 
